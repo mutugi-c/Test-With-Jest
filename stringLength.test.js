e1bb3a5698fbdb68e349e08a9fd400f0c4afcfa4
mutugi-c <mutugichris@gmail.com>
2023-05-10 21:47:19 +0200

    Structure stringLength test with AAA

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,10 +1,24 @@
 const stringLength = require("./stringLength")
 
-test('Hello returns 5', () => {
-    expect(stringLength('Hello')).toBe(5);
+describe('stringLength', () => {
+  test('returns the correct length of a string', () => {
+    // Arrange
+    const str = 'Hello';
+
+    // Act
+    const result = stringLength(str);
+
+    // Assert
+    expect(result).toBe(5);
+  });
+
+  test('throws an error when string length is not between 1 and 10', () => {
+    // Arrange
+    const str = 'Hello World';
+
+    // Act and Assert
+    expect(() => stringLength(str)).toThrow('String length should be between 1 and 10');
+  });
 });
 
-test('An error occurs since stringLength is not between 0 and 10', () => {
-    expect(() => stringLength('Hello World')).toThrow('String length should be between 1 and 10');
-})
 
